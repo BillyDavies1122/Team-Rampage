f61239c9bd80b799ac45add8bb7b508115fc6549
BillyDavies1122 <36976243+BillyDavies1122@users.noreply.github.com>
2018-05-17 23:29:55 +0930

    some script

---
+++ Final Site/scripts/ourScripts.js
@@ -0,0 +1,7 @@
+var dropMenu = document.querySelector('.dropMenu');
+
+var menuClicked = function(evt) {
+  /*if (document.querySelector(".Content").style.display == "hide";*/
+};
+
+/*dropMenu.addEventListener('click',menuClicked);*/
