2b1ef2112425afa6e4263d6ce5a98372e7bbae9c
rahmanashik <36976260+rahmanashik@users.noreply.github.com>
2018-05-23 16:37:59 +0930

    Delete ourScripts.js

--- Final Site/scripts/ourScripts.js
+++
@@ -1,7 +0,0 @@
-var dropMenu = document.querySelector('.dropMenu');
-
-var menuClicked = function(evt) {
-  /*if (document.querySelector(".Content").style.display == "hide";*/
-};
-
-/*dropMenu.addEventListener('click',menuClicked);*/
